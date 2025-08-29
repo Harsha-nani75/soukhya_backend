9e8b5747444a5a2d1c14a1943ca2ccac9810154f
Harsha-nani75 <harsha7595@gmail.com>
2025-08-30 01:35:00 +0530

    n

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ dotenv.config();
 
 // Middleware
 app.use(cors());
-app.use(cors({origin: "http://localhost:4200"}));
+app.use(cors({origin: "https://soukya-jfz5.vercel.app"}));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
